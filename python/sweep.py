f58fb5183c1f5b6b00be03cbeddcac6b9d279725
GEUZE <marsgeuze@gmail.com>
2015-01-09 12:12:29 +0100

    Fixed array initialization of magnitudes
    also fixe some syntax errors

--- python/sweep.py
+++ python/sweep.py
@@ -7,9 +7,9 @@ center_frequency = 4607000
 frequency_span = 2000
 bandwidth = 1000
 data_points = 201
-sweep_count = 1
+sweep_count = 2
 minimum_power = 0
-maximum_power = 0
+maximum_power = 1
 
 ##initialization
 #connect to NA
@@ -20,22 +20,23 @@ inst = rm.open_resource('GPIB0::17::INSTR')
 
 #NA initialization
 inst.write('CENT '+str(center_frequency))  #\set the frequency range of the sweep
-inst.write('SPAN '+str(frequency_span))     #/
-inst.write('BWAUTO OFF')    #\set the IF bandwidth
+inst.write('SPAN '+str(frequency_span))    #/
+inst.write('BWAUTO OFF')                #\set the IF bandwidth
 inst.write('BW '+str(bandwidth))        #/
 inst.write('FMT LINM')  #set NA screen format
-inst.write('POIN '+str(data_points)  #set number of points
+inst.write('POIN '+str(data_points))  #set number of points
 
 #data initialization
 
 amplitudes = numpy.zeros([sweep_count,2*data_points])
 amplitudes_real = numpy.zeros([sweep_count,data_points])
-amplitudes_imaginary = numpy.zeros([sweep_count,data_points)]
+amplitudes_imaginary = numpy.zeros([sweep_count,data_points])
+magnitudes = numpy.zeros([sweep_count,data_points])
 
-power = linspace(minimum_power,maximum_power,sweep_count)
+power = numpy.linspace(minimum_power,maximum_power,sweep_count)
 
 for i in range(sweep_count):
-    inst.write('POWE '+str(power(i)))
+    inst.write('POWE '+str(power[i]))
     
     #measurement
     inst.write('CLES') # clear all status registers
