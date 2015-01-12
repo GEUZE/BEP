fe54a983f8191d7d3c1518ae34d188a48d5ffe32
GEUZE <marsgeuze@gmail.com>
2015-01-12 12:55:59 +0100

    Added amplitude jump tracker

--- python/sweep.py
+++ python/sweep.py
@@ -1,15 +1,15 @@
 import visa
 import matplotlib.pyplot as plt
-import numpy 
+import numpy as np
 
 ##parameters
 center_frequency = 4607000
 frequency_span = 2000
-bandwidth = 1000
+bandwidth = 300
 data_points = 201
-sweep_count = 2
+sweep_count = 1
 minimum_power = 0
-maximum_power = 1
+maximum_power = 0
 
 ##initialization
 #connect to NA
@@ -28,12 +28,12 @@ inst.write('POIN '+str(data_points))  #set number of points
 
 #data initialization
 
-amplitudes = numpy.zeros([sweep_count,2*data_points])
-amplitudes_real = numpy.zeros([sweep_count,data_points])
-amplitudes_imaginary = numpy.zeros([sweep_count,data_points])
-magnitudes = numpy.zeros([sweep_count,data_points])
+amplitudes = np.zeros([sweep_count,2*data_points])
+amplitudes_real = np.zeros([sweep_count,data_points])
+amplitudes_imaginary = np.zeros([sweep_count,data_points])
+magnitudes = np.zeros([sweep_count,data_points])
 
-power = numpy.linspace(minimum_power,maximum_power,sweep_count)
+power = np.linspace(minimum_power,maximum_power,sweep_count)
 
 for i in range(sweep_count):
     inst.write('POWE '+str(power[i]))
@@ -50,10 +50,26 @@ for i in range(sweep_count):
     #processing 
     inst.write('CIRF LIN') #set data format
     amplitudes = inst.query_ascii_values('OUTPDATA?')
-    amplitudes_real[i] = numpy.array(amplitudes[::2])
-    amplitudes_imaginary[i] = numpy.array(amplitudes[1::2])
-    magnitudes[i] = numpy.sqrt(numpy.multiply(amplitudes_real[i], amplitudes_real[i]) + numpy.multiply(amplitudes_imaginary[i], amplitudes_imaginary[i])) #quadratic sum of imaginary and real terms
+    amplitudes_real[i] = np.array(amplitudes[::2])
+    amplitudes_imaginary[i] = np.array(amplitudes[1::2])
+    magnitudes[i] = np.sqrt(np.multiply(amplitudes_real[i], amplitudes_real[i]) + np.multiply(amplitudes_imaginary[i], amplitudes_imaginary[i])) #quadratic sum of imaginary and real terms
     inst.write('CLES') # clear all status registers
     
-#print to computer screen
+    #print to computer screen
     plt.plot(magnitudes[i])
+    
+np.save('test_real', amplitudes_real)
+np.save('test_imaginary',amplitudes_imaginary)
+np.save('test_magnitudes',magnitudes)
+
+def minima(data, n):
+    arguments = np.zeros(n)
+    slope = np.diff(data)
+    for i in range(0,n):
+        arguments[i] = np.argmin(slope)
+        slope[arguments[i]] = 0
+    return arguments
+    
+A = np.load('test_magnitudes.npy')
+minima(A[0],1)
+plt.axvline(minima(A[0],1), ymin=0,ymax=max(A[0]),color='r')
